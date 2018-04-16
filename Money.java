a55616a4746824fa81e78dff612bacd2c0c54727
wynslayton <31413227+wynslayton@users.noreply.github.com>
2018-04-16 09:33:19 -0500

    e

---
+++ Money.java
@@ -0,0 +1,18 @@
+public class Money
+{
+    int total = 0;
+    public Money(int a)
+    {
+        amounts(a, 100);
+    }
+    public int amounts(int a){
+        //list all possible ways of making a with 100, 20, 5, and 1 dollar bills
+        
+        
+        //String future = childMaker(Wynant); //returns Proud
+        //String emotion = emotionFinder(Wynant); //returns HAPPY
+        //int strengthToLive = stl(Wynant); //returns Error: Overflow
+        
+        
+    }
+}
